117837abe01e797900a6e0b1fa409e1a8651413a
Francis Clavette <clavette.francis@gmail.com>
2017-07-24 17:01:04 +0000

    done

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,97 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
+module.exports = function (app, io, fetch) {
+	function getStockInformations(titles) {
+		let all = Promise.all(titles.map((val) => fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + val + "&apikey=" + process.env.ALPHA_KEY).then((res) => res.json())));
+		
+		let labels = [];
+		let datasets = [];
+		
+		return all.then((results) => {
+			labels = Object.keys(results[0]["Time Series (Daily)"]).splice(30).reverse();
+			
+			datasets = results.map((title) => {
+				if(title["Error Message"]) {
+					return undefined;
+				}
+				
+				let data = {
+					label: title["Meta Data"]["2. Symbol"],
+					spanGaps: true,
+					data: [],
+					borderColor: getRandomColor(),
+					fill: false
+				};
+				
+				for(let label of labels) {
+					data.data.push(title["Time Series (Daily)"][label] && title["Time Series (Daily)"][label]["4. close"]);
+				}
+				
+				return data;
+			}).filter((val) => val !== undefined);
+			
+			return {
+				labels,
+				datasets
+			};
+		});
 	}
-
-	var clickHandler = new ClickHandler();
-
+	
+	// https://stackoverflow.com/questions/25594478/different-color-for-each-bar-in-a-bar-chart-chartjs
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	}
+	
+	let TITLES = ["AAPL", "GOOGL", "TSLA", "MSFT", "TWTR", "SNAP"];
+	let STOCK_INFO = {
+		labels: [],
+		datasets: []
+	};
+	
+	getStockInformations(TITLES).then((val) => {
+		STOCK_INFO = val;
+		TITLES = val.datasets.map((sub) => sub.label);
+		io.emit("list", STOCK_INFO);
+	});
+	
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+		
+	io.on('connection', function(socket){
+		console.log('a user connected');
+		socket.emit("list", STOCK_INFO);
+		
+		socket.on('disconnect', function(){
+			console.log('user disconnected');
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
+	
+		socket.on("delete", function(title) {
+			let index = TITLES.indexOf(title);
+			
+			if(index != -1) {
+				TITLES.splice(index, 1);
+				STOCK_INFO.datasets.splice(index, 1);
+				io.emit("list", STOCK_INFO);
+			}
 		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
+		
+		socket.on("add", function(title) {
+			if(TITLES.indexOf(title) == -1) {
+				TITLES.push(title);
+				getStockInformations(TITLES).then((val) => {
+					STOCK_INFO = val;
+					io.emit("list", STOCK_INFO);
+				});
+			}
 		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	});
 };
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 'use strict';
+var routes = require('./app/routes/index.js');
+var session = require('express-session');
+var fetch = require("node-fetch");
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
 	secret: 'secretClementine',
@@ -23,12 +17,9 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+routes(app, io, fetch);
 
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+http.listen(port, function(){
+  console.log('listening on *:3000');
 });
